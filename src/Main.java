1f81ad04ba5678a993ef21c367f7bea140035f8c
Marvin <m.baeumee.industries@gmail.com>
2023-12-01 11:21:50 +0100

    Animation

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,69 @@
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Main {
+public class Main extends JFrame{
+    static int ELEMENTS = 65;
     static MyFrame myFrame;
+    static Point[] pixel;
+
+    private static final int xGap = 5;
+    private static final int yGap = 4;
+    private static final int blockWidth = 10;
+    private static final int myFrameWidth = 1000;
+    private static final int myFrameHeight = 500;
+    private static final int myFrameYOffset = 20;
+    private static final int duration = 40;
+
     public static void main(String[] args) {
-        Point[] pixel = new Point[10];
+        pixel = new Point[ELEMENTS];
+
         for (int i = 0; i < pixel.length; i++) {
-            pixel[i] = new Point((i + 1) * (5 + 10), (480 - i * 3));
+            pixel[i] = new Point(0, ((myFrameHeight - myFrameYOffset) - i * yGap));
         }
-        shuffle(pixel);
+        pixel = shuffle(pixel);
+        myFrame = new MyFrame(myFrameWidth, myFrameHeight, blockWidth, xGap, pixel);
         System.out.println(Arrays.toString(pixel));
-        myFrame = new MyFrame(pixel);
         bubbleSort(pixel);
+
+        JButton shuffleButton = new JButton();
+        shuffleButton.setBounds(0,0,100,50);
+        shuffleButton.setText("shuffle");
+        shuffleButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pixel = shuffle(pixel);
+                myFrame.repaint();
+            }
+        });
+        myFrame.add(shuffleButton);
+        myFrame.update(myFrame.getGraphics());
     }
 
     public static void bubbleSort(Point[] pixel) {
-        for (int i = 0; i < pixel.length - 1; i++) {
-            for (int j = 0; j < pixel.length; j++) {
-                if(pixel[j].y < pixel[i + 1].y) {
-                    Point temp = pixel[i + 1];
-                    pixel[i + 1] = pixel[j];
-                    pixel [j] = temp;
-
-
-
+        for (int i = 0; i < pixel.length; i++) {
+            for (int j = 0; j < pixel.length - 1; j++) {
+                if (pixel[j].y > pixel[j + 1].y) {
+                    Point temp = pixel[j + 1];
+                    pixel[j + 1] = pixel[j];
+                    pixel[j] = temp;
+                }
+                try {
+                    Thread.sleep(duration);
                     myFrame.update(myFrame.getGraphics());
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }
     }
 
-    public static void shuffle(Point[] pixel) {
+    public static Point[] shuffle(Point[] pixel) {
         List<Point> pixelList = Arrays.asList(pixel);
         Collections.shuffle(pixelList);
-        pixelList.toArray(pixel);
+        return pixelList.toArray(pixel);
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,22 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyFrame extends JFrame {
+    int width;
+    int height;
+    int blockWidth;
+    int xGap;
     Point[] pixel;
-    public MyFrame(Point[] pixel) {
+
+    public MyFrame(int width, int height, int blockWidth, int xGap ,Point[] pixel) {
+        this.height = height;
+        this.width = width;
+        this.blockWidth =  blockWidth;
+        this.xGap = xGap;
+        this.pixel = pixel;
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(new Dimension(1000, 500));
+        setSize(new Dimension(width, height));
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
-
-        this.pixel = pixel;
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         for (int i = 0; i < pixel.length; i++) {
-            g.fillRect((i + 1) * (5 + 10), pixel[i].y, 10,this.getHeight() + 500);
+            g.fillRect((i + 1) * (xGap + blockWidth), pixel[i].y, blockWidth,height - pixel[i].y);
         }
     }
 }
